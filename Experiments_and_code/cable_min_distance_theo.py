15c2835d8cbbd23116b3c028268a7ef3274a7612
Jordan <j.longval@robotiq.com>
2021-10-13 09:51:24 -0400

    work in progress

---
+++ Experiments_and_code/cable_min_distance_theo.py
@@ -0,0 +1,57 @@
+
+# Importing libs
+from sympy import *
+import matplotlib.pyplot as plt
+from matplotlib import cm
+import numpy as np
+from mpl_toolkits.mplot3d import Axes3D
+# Defining symbols
+r_1, r_2, D, H_1 = symbols('r_1 r_2 D H_1')
+
+#Defining useful functions
+def norm(v:Matrix):
+    return sqrt(v[0,0]**2+v[1,0]**2+v[2,0]**2)
+def cross_prod(v1:Matrix, v2:Matrix):
+    return Matrix(3,1,[v1[1,0]*v2[2,0]-v1[2,0]*v2[1,0],
+                       v1[0,0]*v2[2,0]-v1[2,0]*v2[0,0],
+                       v1[0,0]*v2[1,0]-v1[1,0]*v2[0,0]])
+def dot_prod(v1:Matrix, v2:Matrix):
+    return v1[0,0]*v2[0,0]+v1[1,0]*v2[1,0]+v1[2,0]*v2[2,0]
+
+# Developing the math
+phi = asin((r_1+r_2)/D)
+p1 = Matrix(3,1,[r_1*cos(phi), -r_1*sin(phi),H_1*phi/(2*pi)])
+p2 = Matrix(3,1,[-r_1*cos(phi), -r_1*sin(phi),H_1*(3*pi-phi)/(2*pi)])
+rho_1 = sqrt(H_1**2+4*pi**2*r_1**2)/(2*pi)
+q1 = Matrix(3,1,[-r_1*sin(phi), -r_1*cos(phi),H_1/(2*pi)])
+q2 = Matrix(3,1,[r_1*sin(3*pi-phi), r_1*cos(3*pi-phi),-H_1/(2*pi)])
+u1 = simplify(q1/rho_1)
+u2 = simplify(q2/rho_1)
+Lf = rho_1*sqrt(D**2-(r_2+r_1)**2)/r_1
+full = simplify((Lf**2)*dot_prod(cross_prod(u1,u2),p2-p1)/norm(cross_prod(u1,u2)))
+
+# Turning the full expression into a python function
+f = lambdify([r_1, r_2, D, H_1], full)
+
+
+@np.vectorize
+def return_distance(r1, r2, D, H1):
+    return f(r1, r2, D, H1)
+
+# Creating values for plotting
+res = 0.01
+r1 = 1
+r2 = 2
+dim_indx = r1 + r2
+D = np.arange(dim_indx+res, 1.2*dim_indx, res)
+H1 = np.arange(res, 0.2*dim_indx, res)
+D, H1 = np.meshgrid(D, H1)
+
+val = return_distance(r1, r2, D, H1)
+
+# Drawing the plot
+fig, ax = plt.subplots(subplot_kw={"projection": "3d"})
+surf = ax.plot_surface(D/dim_indx, H1/dim_indx, val/r1, cmap=cm.coolwarm,
+                       linewidth=0, antialiased=False)
+
+plt.show()
--- Experiments_and_code/main.py
+++ Experiments_and_code/main.py
@@ -5,15 +5,50 @@ import matplotlib.pyplot as plt
 # importing data
 data = pd.read_csv('data.csv')
 
-# Calculating the coefficient of friction
+# Calculating the coefficients of friction
 data['mass'] /= 1000
 data['mass'] += 0.0276
-mu_no_groove = (1/np.pi)*np.log((5.7*data['no_groove']+2*(0.03+0.0009)*9.81*data['mass'])/\
-                                (-5.7*data['no_groove']+2*(0.03+0.0009)*9.81*data['mass']))
+radius = 0.03
+cable_radius = 0.0009
+g = 9.81
+gear_ratio = 5.9
+mu_no_groove = (1/np.pi)*np.log(((radius+cable_radius)*data['mass']*g)/((radius+cable_radius)*data['mass']*g - gear_ratio*data['no_groove']))
 print(mu_no_groove)
-#creating  initial plot
-# fig1 = plt.figure()
-# plt.plot(data['mass'],data['no_groove'])
+mu_simple_groove = (1/np.pi)*np.log((radius*data['mass']*g)/(radius*data['mass']*g - gear_ratio*data['simple_groove']))
+print(mu_simple_groove)
+lambda_simple = mu_simple_groove/mu_no_groove
+print(lambda_simple)
+mu_complexe_groove = (1/np.pi)*np.log((radius*data['mass']*g)/(radius*data['mass']*g - gear_ratio*data['complexe_groove']))
+lambda_complexe = mu_complexe_groove/mu_no_groove
+mu_big_groove = (1/np.pi)*np.log(((radius-0.0018)*data['mass']*g)/((radius-0.0018)*data['mass']*g - gear_ratio*data['big_groove']))
+lambda_big = mu_big_groove/mu_no_groove
+
+# Changing the figure font parameters
+plt.rcParams.update({
+    "text.usetex": True,
+    "font.family": "serif",
+    "font.sans-serif": ["Computer modern roman"]})
+
+# Creating the figure
+fig, ax1 = plt.subplots()
+
+color = 'black'
+ax1.set_xlabel('mass [kg]')
+ax1.set_ylabel('$\mu_k$', color = color)
+plt1 = ax1.plot(data['mass'], mu_no_groove, color =color)
+plt2 = ax1.plot(data['mass'], mu_simple_groove, color=color, linestyle='--')
+
+ax2 = ax1.twinx()
+
+color = 'blue'
+ax2.set_ylabel('$\eta$', color=color)  # we already handled the x-label with ax1
+plt3 = ax2.plot(data['mass'], lambda_simple, color=color)
+ax2.tick_params(axis='y', labelcolor=color)
+fig.tight_layout()
+plt.grid(True)
+all_plt = plt1+plt2+plt3
+ax1.legend(all_plt,['$\mu_{k1}$:no groove', '$\mu_{k2}$:simple groove', '$\eta = \mu_{k2}/\mu_{k1}$'])
+plt.show()
 # plt.plot(data['mass'],data['simple_groove'])
 # plt.plot(data['mass'],data['complexe_groove'])
 # plt.plot(data['mass'],data['big_groove'])
